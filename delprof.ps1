e854deade1150bc3c15e1a9ef0b09648d9c763c2
Lucas Dantas <56886569+lucasldantas@users.noreply.github.com>
2025-09-15 16:50:23 -0300

    Update delprof.ps1

--- delprof.ps1
+++ delprof.ps1
@@ -1,17 +1,17 @@
-# Executar como Administrador
+Clear-Host
 
-$exe = 'C:\Program Files (x86)\Windows Resource Kits\Tools\delprof.exe'
+$delprof = "C:\Program Files (x86)\Windows Resource Kits\Tools\delprof.exe"
 $msi = Join-Path $env:TEMP 'delprof.msi'
-$url = 'https://raw.githubusercontent.com/lucasldantas/limpezadedisco/main/delprof.msi'
+$url = "https://raw.githubusercontent.com/lucasldantas/limpezadedisco/main/delprof.msi"
 
-if (-not (Test-Path $exe)) {
+if (-not (Test-Path $delprof)) {
   [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
   Invoke-WebRequest -Uri $url -OutFile $msi -UseBasicParsing
   Start-Process msiexec.exe -ArgumentList "/i `"$msi`" /qn /norestart" -Wait
 }
 
-if (Test-Path $exe) {
-  & $exe /D:4 /Q /I
+if (Test-Path $delprof) {
+  Start-Process cmd.exe -ArgumentList '/c "C:\Program Files (x86)\Windows Resource Kits\Tools\delprof.exe" /D:30 /Q /I' -Wait -WindowStyle Hidden
 } else {
   Write-Error 'delprof.exe não encontrado após a instalação.'
 }
